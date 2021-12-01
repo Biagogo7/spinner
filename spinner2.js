616feeb1b0eb03e9fd1672dc2a4857a51f1cb1da
Adeyemi Atayero <henry.atayero@gmail.com>
2021-11-30 19:19:15 -0600

    updating spinner repo with spinner2

---
+++ spinner2.js
@@ -0,0 +1,16 @@
+
+
+setTimeout(() => {process.stdout.write('\r|   ')}, 100);
+
+setTimeout(() => {process.stdout.write('\r/   ')}, 300);
+
+setTimeout(() => {process.stdout.write('\r-   ')}, 500);
+
+setTimeout(() => {process.stdout.write('\r\\   ')}, 700);
+
+setTimeout(() => {process.stdout.write('\r/   \n')}, 800);
+
+
+// arrayEle.forEach(element => {setTimeout(() => {process.stdout.write('\relement   ')}, 100);
+  
+// });
